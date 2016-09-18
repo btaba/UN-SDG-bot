2eee930a16415c072e364baa871419e6d4b73f83
Inna <ishteinbuk@gmail.com>
2016-09-18 15:39:17 -0400

    changed to nltk

--- unsdgbot/message_processor/sdg_suggester.py
+++ unsdgbot/message_processor/sdg_suggester.py
@@ -1,26 +1,28 @@
 import pickle
-import spacy
-parser = spacy.load('en')
+from nltk.stem import WordNetLemmatizer
 
 class SDGSuggester(object):
     def __init__(self, max_length=3):
         self.keyword_sentence_dict = pickle.load(open("Suggestions/keyword_sentence_dict.p", "rb"))
         self.sentence_keyword_dict = pickle.load(open("Suggestions/sentence_keyword_dict.p", "rb"))
+        self.wnl = WordNetLemmatizer()
         self.MAX_LENGTH = max_length
 
+
     def process_message(self, message_text):
-        parsed = parser(message_text)
+        parsed = message_text.split(" ")
         sent = []
         for word in parsed:
-            sent.append(word.lemma_)
+            sent.append(self.wnl.lemmatize(word))
         sentences = set()
         for kwd in self.keyword_sentence_dict:
             if kwd in sent:
-                sentences.update(kwd_sent[kwd])
+                sentences.update(self.keyword_sentence_dict[kwd])
         sentences_to_return = []
         count = 0
         for sent in sentences:
             sentence = self.sentence_keyword_dict[sent]["sentence"]
+            url = self.sentence_keyword_dict[sent]["links"]
             if len(sentence) > 320:
                 sent_arr = sentence.split(".")
                 mid_idx = len(sent_arr)/2
@@ -28,7 +30,11 @@ class SDGSuggester(object):
                 sentences_to_return.append("".join(sent_arr[mid_idx:]))
             else:
                 sentences_to_return.append(sentence)
-
+            if len(url) > 1:
+                sentences_to_return.append("Go to these links to find out more: " +
+                                           ", ".join(url))
+            elif len(url) > 0:
+                sentences_to_return.append("Go here to find out more: " + " ".join(url))
             count += 1
             if count == self.MAX_LENGTH:
                 break
